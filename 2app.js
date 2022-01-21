d3158bab56e74a97376875be74d97b4791c027e1
Max <chevalier.maxt@gmail.com>
2022-01-21 11:27:39 -0500

    first commit

---
+++ 2app.js
@@ -0,0 +1,9 @@
+
+
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
+
+
---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules
+
+const names = require('./4_names');
+const sayHi = require('./5-utils');
+const data = require('./6-alt')
+// console.log(data);
+require('./7-mind')
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+
